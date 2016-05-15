ebcc58f74d8c1f848595b81450f9008f1f397af4
Chanon Thongsuk <chanon.pingpong@gmail.com>
2016-05-15 20:04:42 +0700

    create Agreement , Permission

--- src/sitwarehouse/SITwarehouse.java
+++ src/sitwarehouse/SITwarehouse.java
@@ -9,21 +9,6 @@ public class SITwarehouse {
     
     public static void main(String[] args) throws SQLException, ClassNotFoundException  {
         
-     /*  GregorianCalendar startDate,endDate;
-                startDate = new GregorianCalendar();
-                endDate = new GregorianCalendar();
-                DateFormat df = DateFormat.getDateInstance(DateFormat.LONG);
-                
-                Date firstDate = startDate.getTime();
-                Date lastDate = endDate.getTime();
-                String fd = df.format(firstDate);
-                String ld = df.format(lastDate);
-                
-                System.out.println(fd);
-                System.out.println(ld);*/
-      //  Employee.createEmployee("jame", "jame", "jame", "jame", "jame", "jame", "jame");
-      //  SITOperation.loginMember("jamemez", "kittisak");
-        //SITOperation.SeachWarehouseSize("SMALL");
-        Agreement.createPermission(3302, "13/06/2016", 200000);
+        
     }
 }
